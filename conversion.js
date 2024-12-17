2b5fa55975ad22d866dcf673776385fb3d6a31bc
David H Ash <d.h.ash999@gmail.com>
2024-12-17 00:08:28 -0500

    INSERT all users at once

--- conversion.js
+++ conversion.js
@@ -5,14 +5,19 @@ const { neon } = require("@neondatabase/serverless");
 const sql = neon(auth.db.url);
 
 // Convert all users stored as .json into a PostgreSQL database
-fs.readdir("./users", (err, files) => {
+fs.readdir("./users", async (err, files) => {
   if (err) {
     console.error('Error reading directory:', err);
     return;
   }
 
-  files.forEach(async file => {
+  let query = `INSERT INTO Users (id, points, fc) VALUES `;
+
+  await files.forEach(async file => {
     let data = JSON.parse(fs.readFileSync(`./users/${file}`));
-    await sql`INSERT INTO Users (id, points, fc) VALUES (${file.substring(0, file.length-5)}, ${data.points}, ${data.fc})`
+    query += `(${file.substring(0, file.length-5)}, ${parseInt(data.points)}, ${data.fc === undefined ? null : `\'${data.fc}\'`}),`;
+    // await sql`INSERT INTO Users (id, points, fc) VALUES (${file.substring(0, file.length-5)}, ${data.points}, ${data.fc})`
   });
+
+  await sql(query.substring(0, query.length-1));
 });
