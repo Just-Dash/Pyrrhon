4cc68e5b00b3d541b0fdb621321ea12be221a631
David H Ash <d.h.ash999@gmail.com>
2024-12-18 21:01:32 -0500

    Allow mods to add/remove free roles

--- commands/role.js
+++ commands/role.js
@@ -1,4 +1,6 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const fs = require('fs');
+const file = require('./file');
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -25,20 +27,72 @@ module.exports = {
             .setDescription('Role to be removed')
             .setRequired(true)
         )
+    )
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('include')
+        .setDescription('Include a role for users to add to themselves')
+        .addRoleOption(option =>
+          option
+            .setName('role')
+            .setDescription('Role to be included')
+            .setRequired(true)
+        )
+    )
+    .addSubcommand(subcommand =>
+      subcommand
+        .setName('exclude')
+        .setDescription('Exclude a role for users to add to themselves')
+        .addRoleOption(option =>
+          option
+            .setName('role')
+            .setDescription('Role to be excluded')
+            .setRequired(true)
+        )
     ),
   async execute(interaction) {
     await interaction.deferReply().catch((err) => console.log(err));
-    const freeRoles = ['748387591942045696', '710670964471496744', '748388197553143859', '909645162991484948', '1092876840894468136', '1085751214123266189', '1041121570350973049', '1195505529053782116', '1004187677136003172', '1251728970085433446', '1135058998022914071'];
-    if (!freeRoles.includes(interaction.options.getRole('role').id)) {
+    const filepath = './info/freeRoles.json';
+    let file = JSON.parse(fs.readFileSync(filepath));
+    let freeRoles = file.roles;
+
+    if ((interaction.options.getSubcommand() === 'add' || interaction.options.getSubcommand() === 'remove') && !freeRoles.includes(interaction.options.getRole('role').id)) {
       await interaction.editReply('Sorry, you can\'t add/remove yourself from that role.').catch((err) => console.log(err));
     }
     else if (interaction.options.getSubcommand() === 'add') {
       interaction.member.roles.add(interaction.options.getRole('role'));
       await interaction.editReply(`${interaction.options.getRole('role').name} role added.`).catch((err) => console.log(err));
     }
-    else {
+    else if (interaction.options.getSubcommand() === 'remove') {
       interaction.member.roles.remove(interaction.options.getRole('role'));
       await interaction.editReply(`${interaction.options.getRole('role').name} role removed.`).catch((err) => console.log(err));
+    }
+    else if (interaction.options.getSubcommand() === 'include') {
+      if (!interaction.member.permissions.has(PermissionFlagsBits.ManageRoles)) {
+        interaction.editReply('Sorry, you can\'t add additional roles to this command.').catch((err) => console.log(err));;
+      }
+      else {
+        if (freeRoles.includes(interaction.options.getRole('role').id)) {
+          interaction.editReply(`${interaction.options.getRole('role').name} is already included in /role.`).catch((err) => console.log(err));
+        }
+        else {
+          freeRoles.push(interaction.options.getRole('role').id);
+          file.roles = freeRoles;
+          fs.writeFileSync(filepath, JSON.stringify(file, null, 2));
+          interaction.editReply(`${interaction.options.getRole('role').name} role added to /role.`).catch((err) => console.log(err));
+        }
+      }
+    }
+    else if (interaction.options.getSubcommand() === 'exclude') {
+      if (!interaction.member.permissions.has(PermissionFlagsBits.ManageRoles)) {
+        interaction.editReply('Sorry, you can\'t remove roles from this command.').catch((err) => console.log(err));
+      }
+      else {
+        freeRoles = freeRoles.filter(role => role !== interaction.options.getRole('role').id);
+        file.roles = freeRoles;
+        fs.writeFileSync(filepath, JSON.stringify(file, null, 2));
+        interaction.editReply(`${interaction.options.getRole('role').name} role removed from /role.`).catch((err) => console.log(err));
+      }
     }
   },
 };
